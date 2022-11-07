35a14fad732811627b16d44de286d65dd845551b
iZeroKim <kimgichuru6@gmail.com>
2022-11-07 16:27:57 +0300

    Use the external package to display a random pair of words

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main(){
   runApp(
@@ -12,15 +13,20 @@ class MyApp extends StatelessWidget{
       );
   @override
   Widget build(BuildContext context) {
+
+    //Generate a random word pair
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: "Welcome to Flutter",
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Welcome to Flutter")
         ),
-        body: const Center(
-          child: Text("Hello, world!"),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+
         ),
+
       )
     );
   }
