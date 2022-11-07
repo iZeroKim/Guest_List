32d2e09524b6588bb192d09cae1e50059f3cfbef
iZeroKim <kimgichuru6@gmail.com>
2022-11-07 16:05:59 +0300

    first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,17 @@ class MyApp extends StatelessWidget{
       );
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      title: "Welcome to Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Welcome to Flutter")
+        ),
+        body: const Center(
+          child: Text("Hello, world!"),
+        ),
+      )
+    );
   }
 
 }
